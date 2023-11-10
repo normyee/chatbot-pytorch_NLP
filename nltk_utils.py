5a17d15f38a2df48628f841fcdddc0b327490d2a
normyee <lunoqro@gmail.com>
2023-11-10 15:25:38 -0300

    chore: implement bag_of_words function

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,4 +1,5 @@
 import nltk
+import numpy as np
 
 # Uncomment the line below if the pre-trained tokenizer is not downloaded
 # nltk.download('punkt')
@@ -22,4 +23,28 @@ def stem(word: str) -> list:
 
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    BoW   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w, in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+
+    return bag
+
+
+# example below on how it works
+# sentence = ["hello", "how", "are", "you"]
+# words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+# bow = bag_of_words(sentence, words)
+
+# print(bow)
+#output should be: [0. 1. 0. 1. 0. 0. 0.]
