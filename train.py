4dbe8cf128bb26bb960ded8a366f4e71bf7f6e47
normyee <lunoqro@gmail.com>
2023-11-10 15:35:35 -0300

    feat: add dataset

--- train.py
+++ train.py
@@ -1,5 +1,9 @@
 import json
 from nltk_utils import tokenize, stem, bag_of_words
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -28,3 +32,40 @@ all_words = sorted(set(all_words))
 
 # same for tags
 tags = sorted(set(tags))
+
+# BoW
+
+# doc
+x_train = []
+# tags
+y_train = []
+
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    x_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label)
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+
+class ChatDataset(Dataset):
+
+    def __init__(self):
+        self.n_samples = len(x_train)
+        self.x_data = x_train
+        self.y_data = y_train
+
+    # dataset[index]
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return self.n_samples
+    
+# Hyperparameters
+batch_size = 8
+
+dataset = ChatDataset()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0)
