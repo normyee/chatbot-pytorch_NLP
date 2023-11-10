d293eed48010ae8ff147442ec114b702eff6cb8c
normyee <lunoqro@gmail.com>
2023-11-10 14:48:03 -0300

    feat: train.py to train data

---
+++ train.py
@@ -0,0 +1,30 @@
+import json
+from nltk_utils import tokenize, stem, bag_of_words
+
+with open('intents.json', 'r') as f:
+    intents = json.load(f)
+
+all_words = []
+tags = []
+# xy will hold both patterns and the text 
+xy = []
+
+for intent in intents['intents']:
+    tag = intent['tag']
+    tags.append(tag)
+    for pattern in intent['patterns']:
+        w = tokenize(pattern)
+        all_words.extend(w)
+        xy.append((w, tag))
+
+# ignore certain words        
+
+ignore_words = ['?', '!', '.', ',']
+
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+
+# sort doc to get only unique words
+all_words = sorted(set(all_words))
+
+# same for tags
+tags = sorted(set(tags))
